5c3ddb3702894c081e0958d28d2d15911e6ecfd5
kencheng <kenny851017@gmail.com>
2017-01-23 18:24:13 -0500

    package name updated

--- ros_speech2text/setup.py
+++ ros_speech2text/setup.py
@@ -5,5 +5,5 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
-setup_args = generate_distutils_setup(packages=['ros_s2t'],package_dir={'': 'include'},)
+setup_args = generate_distutils_setup(packages=['ros_speech2text'],package_dir={'': 'include'},)
 setup(**setup_args)
