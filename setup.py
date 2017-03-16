1ec186cd4591277244b2f472fe7096e628a1331a
Olivier Mangin <olivier@mangin.com>
2017-03-16 15:00:04 -0400

    CMake cleanup and fixes python install.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
-
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+
 # fetch values from package.xml
-setup_args = generate_distutils_setup(packages=['ros_speech2text'],package_dir={'': 'include'},)
+setup_args = generate_distutils_setup(
+    packages=['s2t'])
+
 setup(**setup_args)
