a7a640e66605b36fd786bf69132c2172c8edfe00
Jake Brawer <jake.brawer@yale.edu>
2018-04-04 17:17:58 -0400

    Split up ros and nosetests.

--- test/test_speech_detection.py
+++ test/test_speech_detection.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
 PKG = 'ros_speech2text'
 
+import os
 import unittest
 from unittest import TestCase
-from std_msgs.msg import String
+
 import numpy as np
 import rospkg
-import rospy
-import os
 
+import rospy
 from ros_speech2text.msg import transcript
-from s2t.speech_detection import (StaticSilenceDetector,
-                                  DynamicSilenceDetector,
-                                  normalize, NORMAL_MAXIMUM,
-                                  add_silence)
+from s2t.speech_detection import (NORMAL_MAXIMUM, DynamicSilenceDetector,
+                                  StaticSilenceDetector, add_silence,
+                                  normalize)
+from std_msgs.msg import String
 
 
 class TestNormalize(TestCase):
-
     @staticmethod
     def test_normalize_ones():
-        a = np.ones((5,))
+        a = np.ones((5, ))
         b = NORMAL_MAXIMUM * a
         np.testing.assert_array_equal(normalize(a), b)
 
     @staticmethod
     def test_normalize_twos():
-        a = 2 * np.ones((5,))
-        b = np.ones((5,)) * NORMAL_MAXIMUM
+        a = 2 * np.ones((5, ))
+        b = np.ones((5, )) * NORMAL_MAXIMUM
         np.testing.assert_array_equal(normalize(a), b)
 
     @staticmethod
@@ -42,35 +41,41 @@ class TestNormalize(TestCase):
         np.testing.assert_array_equal(normalize(a), a)
 
     def test_normalize_result_is_int(self):
-        a = (100 * np.random.random((5,))).astype(np.int16)
+        a = (100 * np.random.random((5, ))).astype(np.int16)
         self.assertEqual(normalize(a).dtype, np.int16)
 
 
 class TestAddSilence(TestCase):
     def test_adds_silence_front(self):
         pass
+
     def test_adds_silence_end(self):
         pass
+
 
 class TestUtteranceDetection(TestCase):
     def test_silent_array_with_clean_average_no_utterance(self):
         # there should be no utterance detected
         pass
+
     def test_silent_array_with_random_average_no_utterance(self):
         # there should be no utterance detected
         pass
+
     def test_constant_array_with_clean_average_is_utterance(self):
         # whole array should be classified as utterance
         pass
+
     def test_constant_array_with_high_average_no_utterance(self):
         # there should be no utterance detected
         pass
+
     def test_sin_array_with_clean_average_is_utterance(self):
         # a part of the array should be classified utterance
         pass
 
-class TestStaticSilenceDetector(TestCase):
 
+class TestStaticSilenceDetector(TestCase):
     def test_silent_is_silent(self):
         sd = StaticSilenceDetector(100, 1.)
         a = np.array([0, 0, 0, 0, 0], dtype=np.int16)
@@ -83,7 +88,6 @@ class TestStaticSilenceDetector(TestCase):
 
 
 class TestDynamicSilenceDetector(TestCase):
-
     def test_silent_is_silent_dynamic(self):
         sd = DynamicSilenceDetector(100, min_average_volume=1.)
         a = np.array([0, 0, 0, 0, 0], dtype=np.int16)
@@ -122,36 +126,3 @@ class TestDynamicSilenceDetector(TestCase):
         # Hence sd.avg_volume == 2
         sd.reset_average()
         self.assertEqual(sd.average_volume, 1.5)
-
-class TestSpeechAPI(TestCase):
-    def setUp(self):
-        
-        p = rospkg.RosPack()
-        package_path = p.get_path('ros_speech2text')
-        self.audio_path = os.path.join(package_path,'test','test_audio')
-        self.result = None
-
-    # def tearDown(self):
-    #     self.pub.unregister()
-
-    def callback1(self,recog_result):
-        print "results received"
-        self.result = recog_result.transcript
-
-    def test_sentence1(self):
-        self.pub= rospy.Publisher('/test_input',String,queue_size=10)
-        file_path = os.path.join(self.audio_path,'sentence0.wav')
-        print file_path
-        self.pub.publish(file_path)
-        rospy.Subscriber('ros_speech2text/user_output',transcript,self.callback1)
-        while self.result==None:
-            print "waiting for result"
-            rospy.sleep(1)
-        print self.result
-        self.assertEqual(self.result,'good morning Baxter how are you doing today')
-
-
-if __name__ == '__main__':
-    import rostest
-    rospy.init_node('test_ros_speech2text')
-    rostest.rosrun(PKG, 'test_speech_detection', TestSpeechAPI)
